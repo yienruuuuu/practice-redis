64d8f1e77d4ebe29e9ebfd59ffe8d1a6096fb464
Eric.Lee <a23034@athena.com.tw>
2025-06-30 10:11:44 +0800

    init

---
+++ src/main/java/io/github/yienruuuuu/Main.java
@@ -0,0 +1,12 @@
+package io.github.yienruuuuu;
+
+/**
+ * @author Eric.Lee
+ * Date: 2025/6/30
+ */ //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
